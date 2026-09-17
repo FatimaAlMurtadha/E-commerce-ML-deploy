cce92706463f5a1c254c10c0da1d4dd05240836b
arbazshah52 <arbazhussain067@gmail.com>
2026-09-17 12:19:29 +0200

    import

--- Frontend/ui.py
+++ Frontend/ui.py
@@ -1,2 +1,10 @@
 import time
 from pathlib import Path
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import requests
+import streamlit as st
+from streamlit_lottie import st_lottie, st_lottie_spinner
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
